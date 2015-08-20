62b6b4d02c0c36de2580c3841e77bf35e7448214
Juliano Sanches <juliano.sanches@adp.com>
2015-08-20 15:53:21 -0300

    Adding basic task manipulation.

--- app/app-controller.js
+++ app/app-controller.js
@@ -9,7 +9,28 @@
 
 		var vm = this;
 
-		vm.greetingMsg = 'Hello World Angular from controller!';
+		vm.greetingMsg = 'Angular Tasks';
+		vm.nextTask = {};
+
+		vm.taskList = {
+			title: 'Angular.js Study Group tasks',
+			tasks: [
+				{name: 'Angular.js overview'},
+				{name: 'Hello World app'},
+				{name: 'Advanced Concepts'}
+			]
+		};	
+
+
+		vm.pushTask = function(){						
+			vm.taskList.tasks.push(vm.nextTask);
+			vm.nextTask = {};
+		}
+
+		vm.removeTask = function(task){
+			var index = vm.taskList.tasks.indexOf(task);
+			vm.taskList.tasks.splice(index, 1);
+		};
 
 	}
 	    
